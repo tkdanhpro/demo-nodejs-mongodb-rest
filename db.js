ce39937b66da0856a5791c773ec5c2725c91cbda
danh.ln <danh.ln@gnt.com.vn>
2020-03-10 08:28:29 +0700

    change pass db

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const Joi = require('joi');
 
 //const mongoUrl = "mongodb://127.0.0.1:27017/moneydb";
 
-const mongoUrl = "mongodb://tkdanh1:%40Tranvan2@money1-shard-00-00-j1fvj.mongodb.net:27017,money1-shard-00-01-j1fvj.mongodb.net:27017,money1-shard-00-02-j1fvj.mongodb.net:27017/moneydb?ssl=true&replicaSet=Money1-shard-0&authSource=admin&retryWrites=true&w=majority";
+const mongoUrl = "mongodb://tkdanh1:tranvan2@money1-shard-00-00-j1fvj.mongodb.net:27017,money1-shard-00-01-j1fvj.mongodb.net:27017,money1-shard-00-02-j1fvj.mongodb.net:27017/moneydb?ssl=true&replicaSet=Money1-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 
 module.exports = {
