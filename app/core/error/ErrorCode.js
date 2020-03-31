a205028f86b00569ca25b302e5361fe2806d528a
danh.ln <danh.ln@gnt.com.vn>
2020-03-31 10:59:08 +0700

    update error code messages

--- app/core/error/ErrorCode.js
+++ app/core/error/ErrorCode.js
@@ -1,23 +1,71 @@
 module.exports ={
+    // Có gì đó sai sai! Hãy thử lại!
+    // Ops! Something went wrong. Please try again!
     APPLICATION_ERROR: 500,
+
+    // Đăng nhập thất bại! vui lòng kiểm tra lại!
+    // Login failed! Check authentication credentials
     AUTHENTICATION_FAILED: 511,
+
+    // Email đã tồn tại!
+    // Email already exists!
     EMAIL_ALREADY_EXISTS: 512,
+
+    // Bạn không có quyền truy cập nội dung này!
+    // Permission Denied!
     PERMISSION_DENIED: 513,
 
+    // Tên của bạn không hợp lệ!
+    // Invalid full name!
     INVALID_FULL_NAME: 411,
+
+    // Tên đăng nhập của bạn không hợp lệ!
+    // Invalid username!
     INVALID_USERNAME: 412,
+
+    // Mật khẩu của bạn không hợp lệ!
+    // Invalid password!
     INVALID_PASSWORD: 413,
+
+    // Mật khẩu phải có ít nhất 6 ký tự!
+    // Password length must be greater than 5 characters!
     PASSWORD_LENGTH_REQUIRE: 414,
+
+    // Tên đăng nhập đã tồn tại!
+    // Username alredy exists 
     USERNAME_ALREADY_EXISTS: 415,
+
+    // Tên đăng nhập phải có ít nhất 6 ký tự!
+    // Username must be greater than 5 characters!    
     USERNAME_LENGTH_REQUIRE: 416,
+
+    // Email không đúng định dạng!
+    // Wrong email format!
     WRONG_EMAIL_FORMAT: 417,
 
+    // Xác thực tài khoản google thất bại!
     GOOGLE_VERIFY_FAILED: 601,
+
+    // Xác thực tài khoản facebook thất bại!
     FACEBOOK_VERIFY_FAILED: 602,
 
+    // Không tìm thấy Sổ gạo này!
+    // Note does not found!
     NOTE_NOT_FOUND: 701,
+
+    // Không tìm thấy Giao dịch!
+    // Transaction does not found!
     TRANSACTION_NOT_FOUND: 702,
+
+    // Danh sách thành viên không được để trống!
+    // Members could not be empty!
     MEMBER_NOTE_EMPTY: 703,
+
+    // Giao dịch này đã hoàn thành!
+    // Transaction has completed already! 
     TRANSACTION_COMPLETED: 704,
+
+    // Sổ gạo này đã hoàn thành!
+    // Note has completed already!
     NOTE_COMPLETED_FOUND: 705,
 }
--- app/core/error/PasswordLengthRequireError.js
+++ app/core/error/PasswordLengthRequireError.js
@@ -3,7 +3,7 @@ const ErrorCode = require('./ErrorCode');
 
 class PasswordLengthRequireError extends ApplicationError {
     constructor(message) {
-        super(message || 'Password length must be greater than 6 characters!', ErrorCode.PASSWORD_LENGTH_REQUIRE);
+        super(message || 'Password length must be greater than 5 characters!', ErrorCode.PASSWORD_LENGTH_REQUIRE);
     }
 }
 
